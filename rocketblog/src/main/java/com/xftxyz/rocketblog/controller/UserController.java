6f81f60b5b328b9c8934770596c3192c106cf707
通晓宇宙 <2581011320@qq.com>
2023-03-28 20:37:30 +0800

    body

--- rocketblog/src/main/java/com/xftxyz/rocketblog/controller/UserController.java
+++ rocketblog/src/main/java/com/xftxyz/rocketblog/controller/UserController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.xftxyz.rocketblog.pojo.User;
@@ -38,9 +38,9 @@ public class UserController {
     // }
     // 注册
     @PostMapping("/register")
-    public String register(HttpSession session, @RequestParam("name") String name,
-            @RequestParam("password") String password, @RequestParam("email") String email,
-            @RequestParam("vertify") String vertify) {
+    public String register(HttpSession session, @RequestBody String name,
+            @RequestBody String password, @RequestBody String email,
+            @RequestBody String vertify) {
         // 获取用户输入的验证码
         // String acode = session.getAttribute("code").toString();
         String acode = (String) session.getAttribute("code");
@@ -55,7 +55,7 @@ public class UserController {
 
     // 登录
     @PostMapping("/login")
-    public String login(@RequestParam("email") String email, @RequestParam("password") String password,
+    public String login(@RequestBody String email, @RequestBody String password,
             HttpSession session) {
         User user = userService.login(email, password);
         if (user == null) {
