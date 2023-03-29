8ef7e6aa42deacb10ee932900b7247c06171485c
Lichenyang <1346802467@qq.com>
2023-03-29 14:08:22 +0800

    build simple

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo on
+cd vueblog
 call npm run build
 xcopy /e /y dist\ ..\rocketblog\src\main\resources\static
 cd ..\rocketblog
-./mvnw clean package -DskipTests
+call ./mvnw clean package -DskipTests
+xcopy /y .\target\rocketblog-0.0.1-SNAPSHOT.jar ..
