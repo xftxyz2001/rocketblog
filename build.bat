6d6c67a314b3e7d7375b1c5bc0b7c9a0d5bce6ad
通晓宇宙 <2581011320@qq.com>
2023-03-30 19:01:10 +0800

    bat

--- build.bat
+++ build.bat
@@ -5,4 +5,5 @@ del /f /s /q ..\rocketblog\src\main\resources\static
 xcopy /e /y dist\ ..\rocketblog\src\main\resources\static
 cd ..\rocketblog
 call ./mvnw clean package -DskipTests
-xcopy /y .\target\rocketblog-0.0.1-SNAPSHOT.jar ..
+scp .\target\rocketblog-0.0.1-SNAPSHOT.jar root@8.130.81.23:/root/
+ssh -tt root@8.130.81.23 "./start.sh"
