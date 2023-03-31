20ed726f90f8b065cf14326aad5a9998cc52da52
Lichenyang <1346802467@qq.com>
2023-03-31 21:30:32 +0800

    检测用户登陆状态

---
+++ vueblog/src/views/usersub/frontsub/fronthotsub/left.vue
@@ -0,0 +1,54 @@
+<template style="absolute">
+  <div class="nav" ref="left">
+    <h3 class="mb-2" style="margin-left: 22px">热门</h3>
+    <el-menu
+      active-text-color="#ffd04b"
+      background-color="#fff"
+      class="el-menu-vertical-demo"
+      text-color="#000"
+      @open="handleOpen"
+      @close="handleClose"
+      style="
+        padding: 0;
+        --el-menu-border-color: #fff;
+        position: sticky;
+        top: 10px;
+      "
+    >
+      <router-link :to="{ name: 'hotlatest' }"
+        ><el-menu-item index="1" style="border-top: 1px #c8c9cc solid">
+          <span class="iconfont" style="margin-right: 5px">&#xe8bc;</span>
+          <span>最新发布</span>
+        </el-menu-item></router-link
+      >
+      <router-link :to="{ name: 'hotmostlike' }">
+        <el-menu-item index="2">
+          <span class="iconfont" style="margin-right: 5px">&#xe60f;</span>
+          <span>点赞最多</span>
+        </el-menu-item></router-link
+      >
+    </el-menu>
+  </div>
+</template>
+
+<script setup >
+</script>
+<script>
+export default {
+  name: "left",
+};
+</script>
+
+<style scoped>
+.nav {
+  position: fixed;
+  top: 70px;
+  background-color: #fff;
+  width: 200px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.05);
+}
+.el-menu-item {
+  --el-menu-hover-bg-color: #0a49e8;
+  --el-menu-active-color: #e3c455;
+}
+</style>
---
+++ vueblog/src/views/usersub/frontsub/fronthotsub/middle/hotlatestmiddle.vue
@@ -0,0 +1,74 @@
+<template>
+  <!-- 所有关注日志展示 -->
+  hot最新
+  <el-card v-for="o in 4" :key="o" class="box-card">
+    <template #header>
+      <div class="card-header">
+        <div>
+          <el-avatar
+            :size="32"
+            class="mr-3"
+            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+            style="margin: 12px 5px 0px 20px"
+          />
+          <span
+            class="text-large font-600 mr-3"
+            style="
+              display: inline-block;
+              overflow: hidden;
+              margin: -3px 5px 3px 0px;
+            "
+          >
+            Title
+          </span>
+        </div>
+        <el-button class="button" text style="color: #24acf2"
+          ><el-icon style="margin: 0 2px 1.5px 0"><Plus /></el-icon
+          >关注</el-button
+        >
+      </div>
+    </template>
+
+    <div>
+      <label style="margin-right: 16px">Switch Loading</label>
+
+      <el-switch v-model="loading" />
+    </div>
+    <el-skeleton style="width: 240px" :loading="loading" animated>
+      <template #template> </template>
+      <template #default> </template>
+    </el-skeleton>
+  </el-card>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const loading = ref(true);
+</script>
+<script>
+export default {};
+</script>
+
+
+<style scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 100%;
+  margin-bottom: 10px;
+  /* vertical-align:baseline; */
+}
+</style>
