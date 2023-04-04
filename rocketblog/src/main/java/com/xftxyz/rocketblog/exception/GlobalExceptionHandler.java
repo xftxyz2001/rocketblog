b6576d75679322f3cb6362448168d0a516d695f2
通晓宇宙 <2581011320@qq.com>
2023-04-04 17:40:31 +0800

    debug vue页面更新

--- rocketblog/src/main/java/com/xftxyz/rocketblog/exception/GlobalExceptionHandler.java
+++ rocketblog/src/main/java/com/xftxyz/rocketblog/exception/GlobalExceptionHandler.java
@@ -9,27 +9,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public String handleException(Exception e, Model model) {
-        // String
-        // getLocalizedMessage()
-        // 创建此可抛件的本地化说明。
-        String localizedMessage = e.getLocalizedMessage();
-        // String
-        // getMessage()
         // 返回此可抛出对象的详细信息消息字符串。
         String message = e.getMessage();
-        // StackTraceElement[]
-        // getStackTrace()
         // 提供对 printStackTrace（） 打印的堆栈跟踪信息的编程访问。
         StackTraceElement[] stackTrace = e.getStackTrace();
         String stackTraceString = "";
         for (StackTraceElement stackTraceElement : stackTrace) {
-            stackTraceString += stackTraceElement.toString();
+            stackTraceString += stackTraceElement.toString() + "\n";
         }
-        // String
-        // toString()
         // 返回此可抛件的简短说明。
         String toString = e.toString();
-        model.addAttribute("localizedMessage", localizedMessage);
         model.addAttribute("message", message);
         model.addAttribute("stackTrace", stackTraceString);
         model.addAttribute("toString", toString);
