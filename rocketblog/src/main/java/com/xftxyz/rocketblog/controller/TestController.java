46026c94bdd3745c66c917fa22334ff36d2a899d
通晓宇宙 <2581011320@qq.com>
2023-04-04 18:33:06 +0800

    不应该啊，指犯错误

--- rocketblog/src/main/java/com/xftxyz/rocketblog/controller/TestController.java
+++ rocketblog/src/main/java/com/xftxyz/rocketblog/controller/TestController.java
@@ -1,46 +1,23 @@
 package com.xftxyz.rocketblog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.xftxyz.rocketblog.service.UserService;
 
-import jakarta.servlet.http.HttpSession;
-
-@Controller
+@RestController
 @RequestMapping("/test")
 public class TestController {
 
     @Autowired
     UserService userService;
 
-    @PostMapping("/register")
-    public String register(@RequestParam("name") String name,
-            @RequestParam("password") String password,
-            @RequestParam("email") String email,
-            // @RequestParam("vertify") String vertify,
-            HttpSession session) {
-        // String name = (String) requestBody.get("name");
-        // String password = (String) requestBody.get("password");
-        // String email = (String) requestBody.get("email");
-        // String vertify = (String) requestBody.get("vertify");
-        // // 获取用户输入的验证码
-        // String acode = session.getAttribute("code").toString();
-        // 输出一下接收到的参数
-        // log.info("name:" + name + ",password:" + password + ",email:" + email +
-        // ",vertify:" + vertify);
-        // log.info(session.getId());
-        // String acode = (String) session.getServletContext().getAttribute("code");
-        // log.info("Application中的验证码：" + acode);
-        // if (acode == null || !acode.equals(vertify)) {
-        // return "验证码错误";
-        // }
-        // 注册
-        // String register = userService.register(name, password, email);
-        // return register;
-        return "success";
+    // 测试除0
+    @PostMapping("/by0")
+    public String by0(@RequestParam("num") Float num) {
+        return String.valueOf(num / 0);
     }
 }
