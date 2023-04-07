709bb20aea08fe112b5382af27cb48dab3a5c472
Lichenyang <1346802467@qq.com>
2023-04-07 18:53:12 +0800

    登录修改

--- vueblog/src/views/usersub/frontsub/frontmessage.vue
+++ vueblog/src/views/usersub/frontsub/frontmessage.vue
@@ -1,16 +1,25 @@
 <template>
-    {{ pageInfo.list }}
+  <el-row :gutter="30">
+    <el-col :span="3" :offset="4"
+      ><div class="grid-content ep-bg-purple" />
+      11</el-col
+    >
+    <el-col :span="8" :offset="1"
+      ><div class="grid-content ep-bg-purple-light" />
+      2</el-col
+    >
+  </el-row>
 </template>
 
 <script setup>
 const pageInfo = ref({});
 axios.get("/user/chat/sessions").then((res) => {
-    var result = res.data;
-    if (result.code == 0) {
-        pageInfo.value = result.data;
-    } else {
-        console.log(result.message);
-    }
+  var result = res.data;
+  if (result.code == 0) {
+    pageInfo.value = result.data;
+  } else {
+    console.log(result.message);
+  }
 });
 </script>
 
