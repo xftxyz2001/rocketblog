de345b536e9f5175565487aed47064a2d094b569
通晓宇宙 <2581011320@qq.com>
2023-04-07 14:58:49 +0800

    test消息发送

--- rocketblog/src/main/java/com/xftxyz/rocketblog/controller/TestController.java
+++ rocketblog/src/main/java/com/xftxyz/rocketblog/controller/TestController.java
@@ -2,11 +2,16 @@ package com.xftxyz.rocketblog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.xftxyz.rocketblog.pojo.User;
 import com.xftxyz.rocketblog.service.UserService;
+import com.xftxyz.rocketblog.util.Utils;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/test")
@@ -24,5 +29,14 @@ public class TestController {
     // 空返回
     @GetMapping("/null")
     public void nullReturn() {
+    }
+
+    // 发送消息
+    @GetMapping("/chat/{to}/{msg}")
+    public String testSendMessage(@PathVariable("to") Long toUserid, @PathVariable("msg") String content,
+            HttpSession session) {
+        User user = (User) Utils.currentUser(session);
+        userService.chat(user.getUserid(), toUserid, content);
+        return "消息发送成功";
     }
 }
