e2036531097066f55b19a5964f6cc515136d7421
通晓宇宙 <2581011320@qq.com>
2023-04-08 14:06:41 +0800

    没有与该用户的聊天

---
+++ rocketblog/src/main/java/com/xftxyz/rocketblog/controller/ChatController.java
@@ -0,0 +1,10 @@
+package com.xftxyz.rocketblog.controller;
+
+/**
+ * 私信相关
+ */
+// @RestController
+// @RequestMapping("/chat")
+public class ChatController {
+
+}
---
+++ rocketblog/src/main/java/com/xftxyz/rocketblog/exception/user/NoChatException.java
@@ -0,0 +1,26 @@
+package com.xftxyz.rocketblog.exception.user;
+
+import com.xftxyz.rocketblog.exception.RocketBlogException;
+
+public class NoChatException extends RocketBlogException {
+
+    public NoChatException() {
+    }
+
+    public NoChatException(String message) {
+        super(message);
+    }
+
+    public NoChatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoChatException(Throwable cause) {
+        super(cause);
+    }
+
+    public NoChatException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
